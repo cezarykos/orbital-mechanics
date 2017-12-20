b87796218d7ffaae9f571f0bafe742660d09501b
cezarykos <33199267+cezarykos@users.noreply.github.com>
2017-12-20 14:53:06 +0100

    Update start_opt.py

--- start_opt.py
+++ start_opt.py
@@ -4,8 +4,7 @@ from astropy import units as u
 from astropy import time
 from poliastro.bodies import Earth, Sun, Venus, Jupiter, Uranus
 from poliastro.twobody import Orbit
-from poliastro import iod
-from poliastro import ephem  
+from poliastro import iod 
 
 import transit_opt
 
@@ -26,51 +25,51 @@ def start_date_optimal(H, date0, date1, m, Isp, step):
 
     step_one0 = True        
 
-    print('Launch date, TOF [days], Delta V dV [km/s], Required propellant mass [kg]')
+    print('data startu, TOF [days], Delta V dV [km/s], potrzebna masa paliwa [kg]')
 
     while date0 < date1:        
-        epoch0 = date0.jyear_str
+        epoch0 = date0.jyear
         ss0 = Orbit.circular(Earth, H, epoch=epoch0)        
         vsE = ss0.rv()[1]       
 
         
         dvEV, date_arrivalV, vsV = transit_opt.transit_optimal(date0, transit_minEV, transit_maxEV, 
-                                                   ephem.get_body_ephem(Earth, epoch0), ephem.get_body_ephem(Venus, epoch), vsE, step)
+                                                   Earth, Venus, vsE, step)
 
         dvVJ, date_arrivalJ, vsJ = transit_opt.transit_optimal(date_arrivalV, transit_minVJ, transit_maxVJ, 
-                                                   ephem.get_body_ephem(Venus, epoch), ephem.get_body_ephem(Jupiter, epoch), vsV, step)
+                                                   Venus, Jupiter, vsV, step)
 
-        dvJS, date_arrivalS, vsS = transit_opt.transit_optimal(date_arrivalJ, transit_minJU, transit_maxJU, 
-                                                   ephem.get_body_ephem(Jupiter, epoch), ephem.get_body_ephem(Uranus, epoch), vsJ, step)
+        dvJU, date_arrivalU, vsU = transit_opt.transit_optimal(date_arrivalJ, transit_minJU, transit_maxJU, 
+                                                   Jupiter, Uranus, vsJ, step)
 
         #koszty paliwa:
-        m_pJS = m * (math.exp((dvJS)/ Isp) - 1)
+        m_pJU = m * (math.exp((dvJU)/ Isp) - 1)
         m_pVJ = (m + m_pJS) * (math.exp((dvVJ) / Isp) - 1)
-        m_pEV = (m + m_pJS + m_pVJ) * (math.exp((dvEV) / Isp) - 1)
+        m_pEV = (m + m_pJU + m_pVJ) * (math.exp((dvEV) / Isp) - 1)
 
-        dv_total = (dvEV) + (dvVJ) + (dvJS)       
-        m_p = m_pEV + m_pVJ + m_pJS     
+        dv_total = (dvEV) + (dvVJ) + (dvJU)       
+        m_p = m_pEV + m_pVJ + m_pJU     
 
         
-        print(date0.iso[0:10], ', %i days, %.3f km/s, %i kg' % (int((date_arrivalS - date0).jd),
+        print(date0.iso[0:10], ', %i days, %.3f km/s, %i kg' % (int((date_arrivalU - date0).jd),
                                                                float(dv_total / u.km * u.s), 
                                                                int(m_p / u.kg)))
 
         if step_one0:       
             delta_v = dv_total
             m_prop = m_p
-            v_out = vsS
-            date_out = date_arrivalS
+            v_out = vsU
+            date_out = date_arrivalU
 
             step_one0 = False
         else:
             if dv_total < delta_v:      
                 delta_v = dv_total
                 m_prop = m_p
-                v_out = vsS
+                v_out = vsU
                 date_in = date0
-                date_out = date_arrivalS
+                date_out = date_arrivalU
 
-        date0 += step
+        date0 += step * u.day
 
     return delta_v, v_out, date_in, date_out, m_prop
